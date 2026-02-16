87a16656839b82c7bdf8291b92d58574bb032064
Md Hossain <hossainmd@uchicago.edu>
2026-02-16 00:14:37 -0500

    first commit

---
+++ Unit1PS1.py
@@ -0,0 +1,53 @@
+"""
+Write a function linear_search() to help Winnie the Pooh locate his lost items.
+The function accepts a list items and a target value as parameters. The function
+should return the first index of target in items, and -1 if target is not in items.
+Do not use any built-in functions.
+"""
+
+
+def linear_search(items, target):
+    for i in range(len(items)):
+        if items[i] == target:
+            return i
+    return -1
+
+
+items = ["haycorn", "haycorn", "haycorn", "hunny", "haycorn"]
+target = "hunny"
+# print(linear_search(items, target))
+
+items = ["bed", "blue jacket", "red shirt", "hunny"]
+target = "red balloon"
+# print(linear_search(items, target))
+
+
+def tiggerfy(word):
+    # Only check ALL substrings if original string has changed
+
+    word = word.strip().lower()
+
+    changed = True
+    previous = word
+    while changed:
+        for each in ["t", "i", "gg", "er"]:
+            if each in word:
+                start = word.index(each)
+                word = word[0:start] + word[start + len(each) :]
+
+        if previous == word:
+            changed = False
+        else:
+            previous = word
+
+    print(word)
+
+
+word = "Trigger"
+tiggerfy(word)
+
+word = "eggplant"
+tiggerfy(word)
+
+word = "Choir"
+tiggerfy(word)
---
+++ test.py
@@ -0,0 +1,5 @@
+x = [1, 2, 3]
+
+
+def f(a, b, c):
+    return a + b + c
